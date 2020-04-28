efe53018d56aa39f6d6affe3e9c01dcd453ca6ad
JhordyCaceres <jhordy.caceres@vallegrande.edu.pe>
2020-04-28 05:16:49 -0500

    init

---
+++ sw.js
@@ -0,0 +1,37 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").then(
+      (registration) => {
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      (err) => {
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
+}
+
+const CACHE_NAME = "my-wev-v0";
+const urlsToCache = [
+  "/",
+  "/images/icon-16.png",
+  "/images/icon-32.png",
+  "/images/icon-48.png",
+  "/images/icon-128.png",
+  "/images/icon-144.png",
+  "/manifest.json",
+  "/index.html",
+  "/sw.js",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
